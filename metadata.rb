443024b8e2084a936029756d1203de6603c1e0e8
ZhijunWei <wzj334965317@outlook.com>
2018-12-04 23:20:59 -0500

    Change openstack-dev to openstack-discuss
    Change-Id: I87af0e5744b639b9a2c53f8fb7bbb79deaf5674b

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'openstack-integration-test'
 maintainer       'openstack-chef'
-maintainer_email 'openstack-dev@lists.openstack.org'
+maintainer_email 'openstack-discuss@lists.openstack.org'
 license          'Apache-2.0'
 description      'Installs and configures the Tempest Integration Test Suite'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
